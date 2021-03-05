ee6bf2bdf707bddd6c1b0e60569930886e6a15a2
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-03-05 22:16:29 +0800

    类的集成

--- line.h
+++ line.h
@@ -1,12 +1,35 @@
+//Line类定义
 #include <iostream>
 
 using namespace std;
 
 class Line
 {
+private:
+    int shares;
+    int share_val;
+    int total_val;
+
+//    void setTotalVal(){
+//        total_val = share_val * shares;
+//    }
+
 public:
 //    void sayHello();
-    void printHello(string name);
+    void setShareVal(int share_val);
+    int getShareVal();
+
+    void setShares(int shares);
+    int getShares();
+
+    void showTotalVal(int share_val,int shares);
+
+    void printTotalVal(int tv);
+    void printHello();
+    void setName(string name);
+    string getName();
+
+    void showVal();
 
 
 //private:
