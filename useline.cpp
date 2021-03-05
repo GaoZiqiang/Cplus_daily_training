e223236aefeb18df56d14a9103f05278a97f0698
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-03-05 22:17:14 +0800

    在第三方程序中调用类成员变量和类成员函数，作用域问题

---
+++ useline.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "line.h"
+
+int main(){
+
+    Line line;
+
+    //line.printTotalVal(200);
+    //line.setShareVal(10);
+    line.setShares(5);
+    //line.getShares();
+    cout << "shares by getShares() is " << line.getShares() << endl;
+
+//    cout << "share_val is " << line.getShareVal() << endl;
+//
+//    line.showTotalVal(line.getShareVal(),line.getShares());
+}
