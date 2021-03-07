741b7edcb58e06e47fd84322e355f03449d7f433
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-03-07 10:37:41 +0800

    第三方程序调用/client端

--- usestock.cpp
+++ usestock.cpp
@@ -4,13 +4,14 @@
 
 int main(){
 
-    Line line;
+    Stock stock = Stock(10,7.3);
+    stock.show();
 
     //line.printTotalVal(200);
     //line.setShareVal(10);
-    line.setShares(5);
-    //line.getShares();
-    cout << "shares by getShares() is " << line.getShares() << endl;
+//    line.setShares(5);
+//    //line.getShares();
+//    cout << "shares by getShares() is " << line.getShares() << endl;
 
 //    cout << "share_val is " << line.getShareVal() << endl;
 //
