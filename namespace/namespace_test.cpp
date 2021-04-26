49f15f8224bdb6a5afd9fc296510e0e095ebb116
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-04-26 16:03:20 +0800

    添加函数方法

--- namespace/namespace_test.cpp
+++ namespace/namespace_test.cpp
@@ -7,6 +7,10 @@ namespace car {// 命名空间的定义
     int model;
     int length;
     int width;
+
+    void ShowLength(int len) {
+        cout << "in car namespace:" << len << endl;
+    }
 }
 
 namespace plane {
@@ -17,6 +21,9 @@ namespace plane {
     }
     int length;
     int width;
+    void ShowLength(int len) {
+        cout << "in plane namespace:" << len << endl;
+    }
 }
 
 namespace car {// 添加命名空间的成员
@@ -30,6 +37,11 @@ int main() {
     car::length = 3;
     cout << "the length of car is " << car::length << endl;
     cout << "the length of c is " << c::length << endl;// 使用namespace的别名
+    using namespace car;
+    ShowLength(9);// 前面这个可以不声明namespace
+
+    using namespace plane;
+    plane::ShowLength(10);// 因为前面的namespace car的作用域会与后边的namespace plane的作用域冲突，所以要声明namespace
 
     return 0;
 }
