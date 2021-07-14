7455dabc7e96836c0da008dcbc4e1e93e1379876
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-07-14 10:02:08 +0800

    unordered_map测试

---
+++ unordered_map/traverse_test.cpp
@@ -0,0 +1,19 @@
+/* unordered_map测试程序 */
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    unordered_map<string,string> mymap= {
+            {"gaoziqiang","boy"},
+            {"gaohan","boy"},
+            {"zhaihui","girl"}
+    };
+    // 开始迭代
+    cout << "mymap contains:" << endl;
+    for (auto it = mymap.begin();it != mymap.end();++it) {
+        cout << it->first << ":" << it->second << endl;
+    };
+}
