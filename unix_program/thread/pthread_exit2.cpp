bafeae48aef84afcf6c3a84107a244a6f825da8b
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-10-04 20:42:24 +0800

    解决子线程退出后栈空间被回收问题

--- unix_program/thread/pthread_exit2.cpp
+++ unix_program/thread/pthread_exit2.cpp
@@ -8,6 +8,7 @@ struct foo {
     int c;
     int d;
 };
+// struct foo foo;// 定义struct foo为全局变量
 
 void printfoo(const char *s, const struct foo *fp)
 {
@@ -21,14 +22,30 @@ void printfoo(const char *s, const struct foo *fp)
 
 void * thr_fn1(void *arg)
 {
-    struct foo *foo;
-    foo = (struct foo *)malloc(sizeof(struct foo));
-    //struct foo foo = {1, 2, 3, 4};
+
+    // 方法一：使用全局变量
+//    foo = {1, 2, 3, 4};
+//    printfoo("thread 1:\n", &foo);
+//    pthread_exit((void *)&foo);
+
+    // 方法二：使用堆
+//    struct foo *foo;
+//    foo = (struct foo *)malloc(sizeof(struct foo));
+//
+//    foo->a = 1;
+//    foo->b = 2;
+//    foo->c = 3;
+//    foo->d = 4;
+//
+//    printfoo("thread 1:\n", foo);
+//    pthread_exit((void *)foo);
+
+    // 方法三：使用主线程的栈空间
+    struct foo *foo = (struct foo *)arg;
     foo->a = 1;
     foo->b = 2;
     foo->c = 3;
     foo->d = 4;
-
     printfoo("thread 1:\n", foo);
     pthread_exit((void *)foo);
 }
@@ -41,14 +58,17 @@ void * thr_fn2(void *arg)
 
 int main(void)
 {
+    // 方法三：使用主线程栈空间
+    struct foo foo;
+
     int err;
     pthread_t tid1;
     pthread_t tid2;
     pthread_t tid3;
     struct foo* fp;
-    printf("before tid1: 0x%x\n", (unsigned int)(long)fp);
+    //printf("before tid1: 0x%x\n", (unsigned int)(long)fp);
 
-    err = pthread_create(&tid1, NULL, thr_fn1, NULL);
+    err = pthread_create(&tid1, NULL, thr_fn1, &foo);
     if (err != 0)
         err_quit("can't create thread 1: %s\n", strerror(err));
     // err = pthread_join(tid1, (void*)&fp);
@@ -58,14 +78,14 @@ int main(void)
     printfoo("after tid1:\n", fp);
     sleep(10);
 
-    err = pthread_create(&tid3, NULL, thr_fn1, NULL);
-    if (err != 0)
-        err_quit("can't create thread 1: %s\n", strerror(err));
-    // err = pthread_join(tid1, (void*)&fp);
-    err = pthread_join(tid3, (void**)&fp);
-    if (err != 0)
-        err_quit("can't join with thread 1: %s\n", strerror(err));
-    printfoo("after tid3:\n", fp);
+//    err = pthread_create(&tid3, NULL, thr_fn1, NULL);
+//    if (err != 0)
+//        err_quit("can't create thread 1: %s\n", strerror(err));
+//    // err = pthread_join(tid1, (void*)&fp);
+//    err = pthread_join(tid3, (void**)&fp);
+//    if (err != 0)
+//        err_quit("can't join with thread 1: %s\n", strerror(err));
+//    printfoo("after tid3:\n", fp);
 
 //    printf("parent starting second thread\n");
 //    err = pthread_create(&tid2, NULL, thr_fn2, NULL);
