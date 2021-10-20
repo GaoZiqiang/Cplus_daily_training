a63932c4de0ca5e0873378529b58c358330955a2
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-10-20 16:07:46 +0800

    定时器处理函数

---
+++ compile/extern/t2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "state.h"
+
+int num = 120;
+
+//int main()
+//{
+//    printf("num in t1.cpp: %d\n", num);
+//}
---
+++ unix_program/signal/keepalive/keep_alive.h
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include "lst_timer.h"
+
+#define FD_LIMIT 65535
+#define MAX_EVENT_NUMBER 1024
+#define TIMESLOT 5
+
+extern int pipefd[2];// 声明extern全局变量
+
+class keep_aliver {
+public:
+    keep_aliver();
+    ~keep_aliver();
+
+    int setnonblocking(int fd);
+    void addfd( int epollfd, int fd );
+
+    static void addsig( int sig );
+    void timer_handler(sort_timer_list &sort_timer_lst, int epollfd);
+    static void sig_handler( int sig );
+    static void cb_func( client_data* user_data, int epollfd );
+
+};
