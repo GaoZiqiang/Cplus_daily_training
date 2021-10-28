cd4e3f248dd720dd996f5e249488c5e1ee156ced
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-10-28 08:44:07 +0800

    class与struct的区别--struct中加方法

---
+++ struct/struct_with_function.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct Person
+{
+    Person(string name);// 构造函数
+    void greet(string other_name);// 定义函数
+    string m_name;
+};
+
+// 构造器
+Person::Person(string name)
+{
+    m_name = name;
+}
+
+// 函数定义
+void Person::greet(string other_name)
+{
+    cout << "Hello " << other_name << endl;
+}
+
+int main()
+{
+//    Person student = {"gaoziqiang"};
+    Person student {"gaoziqiang"};
+    student.greet("gaohan");
+//    student.greet("nihao!");
+//
+//    Person m_person("大壮");
+//    m_person.greet("喜子");
+}
+
