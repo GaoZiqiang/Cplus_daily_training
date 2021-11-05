216ec90ce079a9d02589639efaf9abda069a60b1
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-11-05 21:27:38 +0800

    linux定时器

--- unix_program/signal/alarm.c
+++ unix_program/signal/alarm.c
@@ -4,6 +4,11 @@
 #include <signal.h>
 #include <unistd.h>
 
+void func(int signum)
+{
+    printf("捕捉到信号: %d\n", signum);
+}
+
 int main()
 {
     unsigned int ret = 0;
@@ -12,10 +17,12 @@ int main()
     printf("上一次闹钟剩下的时间: %u\n", ret);
 
     sleep(2);
+    // 捕捉超时信号--SIGALRM
+    signal(SIGALRM, func);
     // 重新设置闹钟--覆盖原闹钟
     // 此时的ret值为上一个alarm闹钟剩余的second数
-    ret = alarm(4);
-    printf("上一次闹钟剩下的时间: %u\n", ret);
+//    ret = alarm(4);
+//    printf("上一次闹钟剩下的时间: %u\n", ret);
 
     printf("按下任意键以继续...\n");
     getchar();
--- unix_program/signal/setitimer.c
+++ unix_program/signal/setitimer.c
@@ -10,6 +10,11 @@ void func(int signum)
     printf("捕捉到信号: %d\n", signum);
 }
 
+void func2(int signum)
+{
+    printf("捕捉到信号2: %d\n", signum);
+}
+
 int main()
 {
     int ret = -1;
@@ -17,15 +22,15 @@ int main()
     struct itimerval tmo;
 
     // 配置定时器
-    // 设置第一次触发时间
-    tmo.it_value.tv_sec = 3;
+    // 设置第一次触发时间--在tmo.it_value时间之后开启定时器
+    tmo.it_value.tv_sec = 5;
     tmo.it_value.tv_usec = 0;
 
     // 设置触发周期
-    tmo.it_interval.tv_sec = 2;
+    tmo.it_interval.tv_sec = 1;
     tmo.it_interval.tv_usec = 0;
 
-    // 捕捉超时信号
+    // 捕捉超时信号--SIGALRM
     signal(SIGALRM, func);
     // 设置定时器
     ret = setitimer(ITIMER_REAL, &tmo, NULL);
@@ -35,8 +40,32 @@ int main()
         exit(-1);
     }
 
+    // 设置第二个定时器
+    struct itimerval tmo2;
+
+    // 配置定时器
+    // 设置第一次触发时间
+    tmo2.it_value.tv_sec = 100;
+    tmo2.it_value.tv_usec = 0;
+
+    // 设置触发周期
+    tmo2.it_interval.tv_sec = 20;
+    tmo2.it_interval.tv_usec = 0;
+
+    // 捕捉超时信号--SIGALRM
+    signal(SIGALRM, func2);
+    // 设置定时器
+    ret = setitimer(ITIMER_REAL, &tmo2, NULL);
+    if (-1 == ret)
+    {
+        perror("setitimer");
+        exit(-1);
+    }
+
+
+
     printf("按下任意键以继续...\n");
-    getchar();// 进程收到定时器超时信号SIGALRM后会终止该进程
+    getchar();// 进程收到定时器超时信号SIGALRM后会终止该进程--终止了main主进程--getchar()子进程自然被终止
 
     return 0;
 }
