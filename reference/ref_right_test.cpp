178f540f21a56e3efc6249e1139ca8239a4621ca
gaoziqianghi@163.com <gaoziqianghi@163.com>
2022-07-23 22:47:06 +0800

    右值引用测试

--- reference/ref_right_test.cpp
+++ reference/ref_right_test.cpp
@@ -1,4 +1,5 @@
 // 右值引用测试
+#include <vector>
 
 void f(int& n) {// 左值引用
     n += 1; // 编译失败，const左值引用不能修改指向变量
@@ -9,6 +10,7 @@ void f2(int && n) {// 右值引用
 }
 
 int main() {
-    f(5);
-//    f2(5);
+    int a = 10;
+//    f(5);
+    f2(std::move(a));
 }
