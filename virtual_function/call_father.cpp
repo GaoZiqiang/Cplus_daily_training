d40a5fe5449d53a61e7c574744b7b9a08dc2870e
gaoziqianghi@163.com <gaoziqianghi@163.com>
2022-08-10 22:55:00 +0800

    调用基类虚函数测试

--- virtual_function/call_father.cpp
+++ virtual_function/call_father.cpp
@@ -7,6 +7,7 @@ using namespace std;
 class fa{
 public:
     virtual void show(){cout << "fa" << endl;}
+    void print() {cout << "print" << endl;}
 };
 
 class son : public fa{
@@ -25,21 +26,10 @@ int main()
     fa one;
     fa two;
 
-    /******测试信息******/
-//    printf("子类show函数地址%x\n", *(int *)*(int *)&boy);
-//    printf("父类show函数地址%x\n", *(int *)*(int *)&one);
-//    printf("父类show函数地址%x\n", *(int *)*(int *)&two);
-
-//    int showptr = 0x42d970;    //这个值反汇编得来的，每次编译可能不同，这是fa::show函数的真正内存所在
-//    func fff;
-//    memcpy((char *)&fff, (char*)&showptr, sizeof(int));
-//    fff();
-    /************/
-
-
-    //如下调用
+    // 如下调用
     // 父类的虚函数被子类覆盖后，若指针强行调用父类的虚函数，编译器直接帮你在函数所在区找到函数，
     // 直接调用call，简单暴力而优雅......
-    (*ptrfa).fa::show();
+//    (*ptrfa).fa::show();
+    (*ptrfa).fa::print();
     return 0;
 }
