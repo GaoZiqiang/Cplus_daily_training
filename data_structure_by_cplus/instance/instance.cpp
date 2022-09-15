d99d466bbb06e12fbc5bc7b17dd857d9523975ee
gaoziqianghi@163.com <gaoziqianghi@163.com>
2022-09-15 19:56:03 +0800

    优化--防止复制

--- data_structure_by_cplus/instance/instance.cpp
+++ data_structure_by_cplus/instance/instance.cpp
@@ -7,11 +7,13 @@
 using namespace std;
 
 class Instance {
+    // 禁外部调用构造函数
 public:
     delete Instance();
     delete ~Instance();
 
 public:
+    // 懒汉+DCL双重检查
     static Instance& getInstance() {
         if (instance == nullptr) {
             pthread_mutex_lock(mutex);
@@ -26,5 +28,10 @@ public:
 private:
     Instance* instance;
     Instance();
+    // 禁止复制单例对象
+    Instance(Instance& ins) {};
     ~Instance();
+
+    // 互斥信号量
+    pthread_mutex_t mutex;
 };
